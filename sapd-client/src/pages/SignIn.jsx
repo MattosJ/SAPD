ca283c6ca92ae959bb7e052eeaf27faaaf63bacf
MattosJ <jamespereiramatos23@gmail.com>
2025-10-24 16:12:52 -0300

    chore(): remoção de código padrão do react + criação da pasta de páginas

---
+++ sapd-client/src/pages/SignIn.jsx
@@ -0,0 +1,9 @@
+const SignIn =  () => {
+  return(
+    <>
+    <h1>Página de Login</h1>
+    </>
+  )
+}
+
+export default SignIn;
---
+++ sapd-client/src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+const SignUp =  () => {
+  return(
+    <>
+    <h1>Página de Registro</h1>
+    </>
+  )
+}
+
+export default SignUp;

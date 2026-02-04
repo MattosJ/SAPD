7b418d511e73216d29aa64b8672bb7fc663a73a2
kleverton santos souza <klevertonsouza@academico.ufs.br>
2026-02-04 20:44:26 -0300

    fix#013 acrescentar relacionamento entere alimentos e refeição

--- server/src/repositories/RefeicaoRepository.js
+++ server/src/repositories/RefeicaoRepository.js
@@ -3,15 +3,41 @@ import db from '../database/connection.js';
 class RefeicaoRepository {
 
   async criar(dados) {
-    const r = await db.query(
-      `
-      INSERT INTO refeicoes (usuario_id, tipo, data_hora)
-      VALUES ($1,$2,$3)
-      RETURNING *
-      `,
-      [dados.usuario_id, dados.tipo, dados.data_hora]
-    );
-    return r.rows[0];
+
+        // cria refeição
+        const refeicao = await db.query(
+          `
+          INSERT INTO refeicoes (usuario_id, tipo, data_hora)
+          VALUES ($1,$2,$3)
+          RETURNING *
+          `,
+          [dados.usuario_id, dados.tipo, dados.data_hora]
+        );
+
+        const refeicaoCriada = refeicao.rows[0];
+
+
+        if (dados.alimentos && dados.alimentos.length > 0) {
+          for (const alimento of dados.alimentos) {
+
+            await db.query(
+              `
+              INSERT INTO refeicao_alimentos
+              (refeicao_id, alimento_id, quantidade)
+              VALUES ($1,$2,$3)
+              `,
+              [
+                refeicaoCriada.id,
+                alimento.id,
+                alimento.quantidade
+              ]
+            );
+
+          }
+
+        }
+
+        return refeicaoCriada;
   }
 
   async listarPorUsuario(usuario_id) {
